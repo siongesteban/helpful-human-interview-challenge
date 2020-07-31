4b02635eb4b9426abe28a3890aefd9fd12b75d9f
siongesteban <esteban@mclinica.com>
2020-07-31 16:50:02 +0800

    Setup module-alias

--- api/src/modules/app/app.module.ts
+++ api/src/modules/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { ConfigModule } from '../../shared/config';
-import { GraphQLModule } from '../../shared/graphql';
+import { ConfigModule } from '@shared/config';
+import { GraphQLModule } from '@shared/graphql';
 
-import { SwatchModule } from '../swatch';
+import { SwatchModule } from '@modules/swatch';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
