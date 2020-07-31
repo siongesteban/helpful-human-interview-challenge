1d71d7c9daba1b1e698a9261f2dc0f75d00ecd88
siongesteban <esteban@mclinica.com>
2020-07-31 17:29:36 +0800

    Create initial prisma module

--- api/src/modules/app/app.module.ts
+++ api/src/modules/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '@shared/config';
 import { GraphQLModule } from '@shared/graphql';
+import { PrismaModule } from '@shared/prisma';
 
 import { SwatchModule } from '@modules/swatch';
 
@@ -9,7 +10,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule, GraphQLModule, SwatchModule],
+  imports: [ConfigModule, GraphQLModule, PrismaModule, SwatchModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/modules/swatch/resolvers/color.resolver.ts
+++ api/src/modules/swatch/resolvers/color.resolver.ts
@@ -1,11 +1,15 @@
 import { Resolver, Query } from '@nestjs/graphql';
 
+import { PrismaService } from '@shared/prisma/services';
+
 import { Color } from '../models';
 
 @Resolver(() => Color)
 export class ColorResolver {
+  constructor(private readonly prismaService: PrismaService) {}
+
   @Query(() => Color, { name: 'color' })
   async getColor(): Promise<Color> {
-    return { name: 'red' };
+    return this.prismaService.color.findOne({ where: { id: 1 } });
   }
 }
