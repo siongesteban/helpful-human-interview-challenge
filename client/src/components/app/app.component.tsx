909f348c5b88ba3a943a8301bcbafefd020c28fd
siongesteban <esteban@mclinica.com>
2020-08-02 23:31:47 +0800

    Create initial global style

---
+++ client/src/components/app/app.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { S } from './app.styles';
+
+export const App: React.FC = () => (
+  <>
+    <S.GlobalStyle />
+    <p>
+      The <strong>quick brown fox</strong> jumps over the lazy dog
+    </p>
+  </>
+);
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Normalize } from 'styled-normalize';
 
-import App from './App';
+import { App } from 'components/app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Normalize />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
