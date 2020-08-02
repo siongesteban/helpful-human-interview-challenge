bb09a4c33c8046454161ecee924816cfc130e0c0
siongesteban <esteban@mclinica.com>
2020-08-02 23:42:27 +0800

    Restructure components by module

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from 'components/app';
+import { App } from 'modules/app/components';
 
 ReactDOM.render(
   <React.StrictMode>
