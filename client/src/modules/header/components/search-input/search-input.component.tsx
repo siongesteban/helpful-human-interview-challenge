a8944697434b1e6ff885dbf8d29daa9ab31bf330
siongesteban <esteban@mclinica.com>
2020-08-05 01:03:22 +0800

    Persist search as query param

--- client/src/modules/header/components/search-input/search-input.component.tsx
+++ client/src/modules/header/components/search-input/search-input.component.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { debounce } from 'lodash-es';
+
+import { useQueryParams } from 'shared/hooks';
 
 import { S } from './search-input.styles';
 
-export const SearchInput: React.FC = () => <S.Wrapper placeholder="Search" />;
+export const SearchInput: React.FC = () => {
+  const { queryParams, setQueryParams } = useQueryParams<{ search: string }>();
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = ({
+    target,
+  }) => {
+    debounce(() => {
+      setQueryParams({ search: target.value || null });
+    }, 500)();
+  };
+
+  return (
+    <S.Wrapper
+      placeholder="Search"
+      onChange={handleChange}
+      defaultValue={queryParams.search}
+    />
+  );
+};
