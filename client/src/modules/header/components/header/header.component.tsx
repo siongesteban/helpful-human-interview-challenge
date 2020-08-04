d952a7f96d162468941bfe19409d733c881b90a4
siongesteban <esteban@mclinica.com>
2020-08-05 02:53:00 +0800

    Hide SearchInput if not in list page

--- client/src/modules/header/components/header/header.component.tsx
+++ client/src/modules/header/components/header/header.component.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useLocation } from '@reach/router';
 
 import { ReactComponent as Logo } from 'shared/assets/svg/logo.svg';
 
 import { SearchInput } from '../search-input';
 import { S } from './header.styles';
 
-export const Header: React.FC = () => (
-  <S.Wrapper>
-    <Logo />
-    <SearchInput />
-  </S.Wrapper>
-);
+export const Header: React.FC = () => {
+  const showSearchInput = useLocation().pathname === '/';
+
+  return (
+    <S.Wrapper>
+      <Logo />
+      {showSearchInput && <SearchInput />}
+    </S.Wrapper>
+  );
+};
