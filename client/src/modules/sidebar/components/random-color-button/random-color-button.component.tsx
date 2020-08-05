f6e5d0b01cb07d0517b6ef0382ed4c75bc4e90ef
siongesteban <esteban@mclinica.com>
2020-08-05 19:16:15 +0800

    Disable RandomColorButton instead of hiding

--- client/src/modules/sidebar/components/random-color-button/random-color-button.component.tsx
+++ client/src/modules/sidebar/components/random-color-button/random-color-button.component.tsx
@@ -10,18 +10,14 @@ export const RandomColorButton: React.FC = () => {
   const paginatedSwatchesStore = useGetPaginatedSwatchesStore();
   const totalNumberOfSwatches = paginatedSwatchesStore?.meta.totalCount;
 
-  if (!totalNumberOfSwatches) {
-    return null;
-  }
-
   const handleClick = (): void => {
-    const randomSwatchId = random(1, totalNumberOfSwatches);
+    const randomSwatchId = random(1, totalNumberOfSwatches || 1);
 
     navigate(`/swatches/${randomSwatchId}`);
   };
 
   return (
-    <Button fullWidth onClick={handleClick}>
+    <Button disabled={!totalNumberOfSwatches} fullWidth onClick={handleClick}>
       Random Color
     </Button>
   );
