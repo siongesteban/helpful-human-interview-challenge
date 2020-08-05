2da8c3462fea8a0de0b6bed1f60c56e1c388860b
siongesteban <esteban@mclinica.com>
2020-08-05 16:59:04 +0800

    Create active styles for page number

--- client/src/shared/components/pagination/pagination.component.tsx
+++ client/src/shared/components/pagination/pagination.component.tsx
@@ -6,34 +6,47 @@ import { useQueryParams } from 'shared/hooks';
 import { S } from './pagination.styles';
 
 export interface PaginationProps {
+  readonly currentPageNumber: number;
   readonly pageCount: number;
 }
 
 export interface ItemProps {
+  readonly active: boolean;
   readonly pageNumber: number;
   readonly onClick: (pageNumber: number) => void;
 }
 
-const Item: React.FC<ItemProps> = ({ pageNumber, onClick }) => {
+const Item: React.FC<ItemProps> = ({ active, pageNumber, onClick }) => {
   const handleClick = (): void => {
     onClick(pageNumber);
   };
 
-  return <S.Number onClick={handleClick}>{pageNumber}</S.Number>;
+  return (
+    <S.Number active={active} onClick={handleClick}>
+      {pageNumber}
+    </S.Number>
+  );
 };
 
-export const Pagination: React.FC<PaginationProps> = ({ pageCount }) => {
+export const Pagination: React.FC<PaginationProps> = ({
+  currentPageNumber,
+  pageCount,
+}) => {
   const { setQueryParams } = useQueryParams();
 
   const handleClick = (pageNumber: number): void => {
     setQueryParams({ page: pageNumber });
   };
 
-  return (
-    <S.Wrapper>
-      {range(1, pageCount + 1).map((_, i) => (
-        <Item pageNumber={i + 1} onClick={handleClick} />
-      ))}
-    </S.Wrapper>
-  );
+  const renderItems = (): JSX.Element[] =>
+    range(1, pageCount + 1).map((_, i) => {
+      const pageNumber = i + 1;
+      const active = pageNumber === currentPageNumber;
+
+      return (
+        <Item active={active} pageNumber={pageNumber} onClick={handleClick} />
+      );
+    });
+
+  return <S.Wrapper>{renderItems()}</S.Wrapper>;
 };
--- client/src/shared/components/pagination/pagination.styles.ts
+++ client/src/shared/components/pagination/pagination.styles.ts
@@ -1,17 +1,36 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface NumberProps {
+  active: boolean;
+}
+
+const numberBorderBottom = css`
+  border-bottom: 2px solid #363c3c;
+`;
+
+const getNumberActiveStyles = ({ active }: NumberProps) =>
+  !active
+    ? undefined
+    : css`
+        font-weight: bold;
+        ${numberBorderBottom}
+      `;
 
 const Wrapper = styled.div`
   margin: 0 auto;
 `;
 
-const Number = styled.span`
+const Number = styled.span<NumberProps>`
   cursor: pointer;
   font-size: 24px;
-  margin: 0 15px;
+  margin: 0 10px;
+  padding: 0 5px;
 
   &:hover {
-    text-decoration: underline;
+    ${numberBorderBottom}
   }
+
+  ${getNumberActiveStyles}
 `;
 
 export const S = {
